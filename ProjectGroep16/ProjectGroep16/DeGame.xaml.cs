7cd6d9cc08883d1c36a80bb3ca14066c3bf349f7
Mahmoud-Aljumaa <147814307+Mahmoud-Aljumaa@users.noreply.github.com>
2023-10-16 15:03:27 +0200

    GameWindow (ThemaWindow) De Game (PuzzelSpel)
    Hierbij is de start van het ontwikkelen van twee schermen (Thema- en DeSpelscherm)

---
+++ ProjectGroep16/ProjectGroep16/DeGame.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PuzzelGame
+{
+    public partial class DeGame : Window
+    {
+        public DeGame()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var clickedButton = (Button)sender;
+
+            // Controleer of het geklikte knopje leeg is
+            if (clickedButton.Content.ToString() == "")
+                return;
+
+            // Vind de rij en kolom van het geklikte knopje in het Grid
+            int row = -1;
+            int col = -1;
+            foreach (UIElement element in GameGrid.Children)
+            {
+                if (element is Button button)
+                {
+                    if (button == clickedButton)
+                    {
+                        row = Grid.GetRow(button);
+                        col = Grid.GetColumn(button);
+                        break;
+                    }
+                }
+            }
+
+            if (row == -1 || col == -1)
+                return;
+
+            // Controleer of het geklikte knopje naast het lege vakje ligt
+            int emptyRow = Grid.GetRow(EmptyButton);
+            int emptyCol = Grid.GetColumn(EmptyButton);
+            if ((Math.Abs(row - emptyRow) == 1 && col == emptyCol) || (Math.Abs(col - emptyCol) == 1 && row == emptyRow))
+            {
+                // Ruil de inhoud van de geklikte knop en het lege vakje
+                string temp = clickedButton.Content.ToString();
+                clickedButton.Content = "";
+                EmptyButton.Content = temp;
+                EmptyButton.IsEnabled = true;
+                clickedButton.IsEnabled = true;
+            }
+
+            // Voer hier de logica voor het controleren op winstcondities uit (bijvoorbeeld controleren of de stukjes in de juiste volgorde staan).
+        }
+
+        private Button? EmptyButton
+        {
+            get
+            {
+                foreach (UIElement element in GameGrid.Children)
+                {
+                    if (element is Button button && button.Content.ToString() == "")
+                    {
+                        return button;
+                    }
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ ProjectGroep16/ProjectGroep16/GameWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PuzzelGame
+{
+    /// <summary>
+    /// Interaction logic for GameWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartGame_Click(object sender, RoutedEventArgs e)
+        {
+            GameWindow gameWindow = new GameWindow();
+            gameWindow.Show();
+            this.Close(); // Sluit het huidige venster (optioneel).
+        }
+        private void Image1_Click(object sender, RoutedEventArgs e)
+        {
+            DeGame deGame = new DeGame();
+            deGame.Show();
+            this.Close();
+        }
+
+        private void Image2_Click(object sender, RoutedEventArgs e)
+        {
+            // Voeg hier de code toe om naar het gewenste venster te gaan wanneer op de tweede afbeelding wordt geklikt.
+        }
+
+        private void Image3_Click(object sender, RoutedEventArgs e)
+        {
+            // Voeg hier de code toe om naar het gewenste venster te gaan wanneer op de derde afbeelding wordt geklikt.
+        }
+    }
+}
