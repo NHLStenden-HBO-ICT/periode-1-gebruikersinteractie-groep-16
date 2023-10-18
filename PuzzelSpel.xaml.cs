697cc915990b61daf48cd79e6e0b5600eb400689
ToufikCE <147702212+ToufikCE@users.noreply.github.com>
2023-10-18 11:53:12 +0200

    Add files via upload

---
+++ PuzzelSpel.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for PuzzelSpel.xaml
+    /// </summary>
+    public partial class PuzzelSpel : Window
+    {
+        public PuzzelSpel()
+        {
+            InitializeComponent();
+        }
+
+        private void Openmoeilijkheidsgraad(object sender, RoutedEventArgs e)
+        {
+            MainWindow objsecondwindow = new();
+            this.Visibility = Visibility.Hidden;
+            objsecondwindow.Show();
+        }
+    }
+}
---
+++ Thema.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Thema.xaml
+    /// </summary>
+    public partial class Thema : Window
+    {
+        public Thema()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenMoelijkheidsgraad(object sender, RoutedEventArgs e)
+        {
+            MainWindow objsecondwindow = new();
+            this.Visibility = Visibility.Hidden;
+            objsecondwindow.Show();
+        }
+    }
+}
