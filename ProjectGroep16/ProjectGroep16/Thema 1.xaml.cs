af315d5353344629ad831aa2be6bf2f8cb5b0f48
rickvelting <147702142+rickvelting@users.noreply.github.com>
2023-10-30 10:11:30 +0100

    Add files via upload

---
+++ ProjectGroep16/ProjectGroep16/Thema 1.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Automation.Provider;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rick_Thema
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void VorigeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 objWindow1 = new Window1();
+            this.Visibility = Visibility.Hidden;
+            objWindow1.Show();
+        }
+
+        private void VolgendeButton_Click(object sender, RoutedEventArgs e)
+        {
+           Window1 objWindow1 = new Window1();
+            this.Visibility = Visibility.Hidden;
+            objWindow1.Show();
+        }
+
+        private void FotoButton_Click(object sender, RoutedEventArgs e)
+        {
+            spel objWindow1 = new spel();
+            this.Visibility = Visibility.Hidden;
+            objWindow1.Show();
+        }
+
+        private void Foto2Button_Click(object sender, RoutedEventArgs e)
+        {
+            spel objWindow1 = new spel();
+            this.Visibility = Visibility.Hidden;
+            objWindow1.Show();
+        }
+
+        private void Foto3Button_Click(object sender, RoutedEventArgs e)
+        {
+            spel objWindow1 = new spel();
+            this.Visibility = Visibility.Hidden;
+            objWindow1.Show();
+        }
+
+        private void Foto2Button_DpiChanged(object sender, DpiChangedEventArgs e)
+        {
+
+        }
+    }
+
+   
+    
+    
+}
---
+++ ProjectGroep16/ProjectGroep16/Thema2.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rick_Thema
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void FotoButton_Click(object sender, RoutedEventArgs e)
+        {
+            spel objWindow1 = new spel();
+            this.Visibility = Visibility.Hidden;
+            objWindow1.Show();
+        }
+
+        private void Foto2Button_Click(object sender, RoutedEventArgs e)
+        {
+            spel objWindow1 = new spel();
+            this.Visibility = Visibility.Hidden;
+            objWindow1.Show();
+        }
+
+        private void Foto3Button_Click(object sender, RoutedEventArgs e)
+        {
+            spel objWindow1 = new spel();
+            this.Visibility = Visibility.Hidden;
+            objWindow1.Show();
+        }
+
+        private void VorigeButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow objWindow1 = new MainWindow();
+            this.Visibility = Visibility.Hidden;
+            objWindow1.Show();
+        }
+
+        private void VolgendeButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow objWindow1 = new MainWindow();
+            this.Visibility = Visibility.Hidden;
+            objWindow1.Show();
+
+        }
+    }
+}
